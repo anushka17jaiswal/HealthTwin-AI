8838a1c9e80155010da5b736897f2ede4bbd4f95
ANUSHKA JAISWAL <anushka17jaiswal@gmail.com>
2026-06-23 20:16:24 +0530

    Update main.py

--- Documents/HealthTwin AI/backend/main.py
+++ Documents/HealthTwin AI/backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     allow_origins=[
       "http://localhost:5173",
         "https://healthtwin-ai-2.onrender.com",
-        "https://health-twin-ai-lime.vercel.app/chat",
+        "https://health-twin-ai-lime.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
